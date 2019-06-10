ac204c1b29cb32bee096ba3d6dc8288d160d023e
Younghee Kwon <youngheekwon@ymail.com>
2019-06-10 10:39:24 -0700

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,23 @@ class PostsController < ApplicationController
   def index
   end
 
-  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date,:rationale))
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+
+    
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,20 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
 	end
+
+  describe 'creation' do
+    it 'has a form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new from page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+      expect(page).to have_content("Some rationale")
+    end
+  end
 end
